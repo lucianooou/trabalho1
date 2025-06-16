aa12412808a9611e673ba59e47934c6ec00492b8
lucianooou <lucianoo.9918@gmail.com>
2025-06-16 20:26:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let paragrafo = document.querySelector("#mensagem");
+
+let button = document.querySelector("#botao");
+
+button.addEventListener("click", boasvindas);
+
+function boasvindas(){
+    button.innerHTML="welcome";
+}
+
+
+
+
+
+
+
+
+            
